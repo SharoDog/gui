10d6bd5a45ae39e62da2974a04b8da2741166599
Kaloyan Dimitrov <kaloyankd.06@gmail.com>
2023-05-06 21:38:00 +0300

    Added face detection.

--- src/video/video.cpp
+++ src/video/video.cpp
@@ -1,8 +1,9 @@
 #include "video.h"
-#include <opencv2/imgcodecs.hpp>
 
 ImageProvider::ImageProvider()
-    : QQuickImageProvider(QQuickImageProvider::Image), _socket() {
+    : QQuickImageProvider(QQuickImageProvider::Image), _socket(),
+      _faceDetector(
+          cv::FaceDetectorYN::create("yunet.onnx", "", cv::Size(640, 480))) {
   _socket.bind(QHostAddress::Any, 9999);
   connect(&_socket, &QUdpSocket::readyRead, this,
           &ImageProvider::processDatagrams);
@@ -21,6 +22,21 @@ void ImageProvider::processDatagrams() {
     _socket.readDatagram(datagram.data(), datagram.size());
   }
   auto frame = cv::imdecode(datagram, cv::IMREAD_COLOR);
+  _faceDetector->setInputSize(cv::Size(frame.cols, frame.rows));
+  cv::Mat faces;
+  _faceDetector->detect(frame, faces);
+  for (int i = 0; i < faces.rows; i++) {
+    cv::rectangle(
+        frame,
+        cv::Rect2i(int(faces.at<float>(i, 0)), int(faces.at<float>(i, 1)),
+                   int(faces.at<float>(i, 2)), int(faces.at<float>(i, 3))),
+        cv::Scalar(0, 255, 0), lineThickness);
+    cv::putText(frame, cv::format("%.2f", faces.at<float>(i, 14)),
+                cv::Point2i(int(faces.at<float>(i, 0)),
+                            int(faces.at<float>(i, 1) - 10)),
+                cv::FONT_HERSHEY_SIMPLEX, 0.8, cv::Scalar(0, 255, 0),
+                lineThickness, cv::LINE_AA);
+  }
   updateImage(std::make_shared<QImage>(
       QImage(frame.data, frame.cols, frame.rows, QImage::Format_RGB888)));
 }
--- src/video/video.h
+++ src/video/video.h
@@ -3,12 +3,19 @@
 #include <QUdpSocket>
 #include <QtQuick/QQuickImageProvider>
 #include <iostream>
+#include <opencv2/dnn.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/objdetect.hpp>
+
+const int lineThickness = 2;
 
 class ImageProvider : public QQuickImageProvider {
   Q_OBJECT
 private:
   QUdpSocket _socket;
   std::shared_ptr<QImage> _image;
+  cv::Ptr<cv::FaceDetectorYN> _faceDetector;
 
 public:
   explicit ImageProvider();
